08c694500962e27f5e2f9d31c0a4075ad0070bdd
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-11-01 22:05:09 -0400

    feat: path validations

--- src/main.rs
+++ src/main.rs
@@ -28,4 +28,42 @@ fn main() {
         }
         i += 1;
     }
+
+    // Check if the arguments are empty
+    if backup_dir == "" {
+        println!("Error: -b requires a directory to backup");
+        std::process::exit(1);
+    }
+    if source_dir == "" {
+        println!("Error: -s requires a directory to source");
+        std::process::exit(1);
+    }
+
+    // Convert to absolute paths
+    let backup_dir = fs::canonicalize(backup_dir).unwrap();
+    let source_dir = fs::canonicalize(source_dir).unwrap();
+
+    // Check if the paths are directories
+    if !backup_dir.is_dir() {
+        println!(
+            "Error: the backup path is is not a directory, {}",
+            backup_dir.display()
+        );
+        std::process::exit(1);
+    }
+    if !source_dir.is_dir() {
+        println!(
+            "Error: the source path is not a directory, {}",
+            source_dir.display()
+        );
+        std::process::exit(1);
+    }
+
+    // Check if the paths are the same
+    if source_dir == backup_dir {
+        println!("Error: the source and backup paths are the same");
+        std::process::exit(1);
+    }
+
+    //
 }
