478e57a1c8ea018cf5d8580b40d8b5cd4dc4946a
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-11-04 15:59:44 -0400

    refactor: remove_file.rs to simplify imports.

--- src/utils/remove_file.rs
+++ src/utils/remove_file.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 pub fn remove_file(source_base_path: PathBuf, from_path: PathBuf, to_directory: PathBuf) {
     let relative_path = from_path.strip_prefix(&source_base_path).unwrap();
