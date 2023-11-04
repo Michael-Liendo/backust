c071cd7915a074357189ea0c08f0ecf984312394
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-11-04 01:33:13 -0400

    refactor: refactor remove_file function to handle directories recursively.

--- src/utils/remove_file.rs
+++ src/utils/remove_file.rs
@@ -1,11 +1,24 @@
 use std::{fs, path::PathBuf};
 
-pub fn remove_file(from_path: String, to_directory: PathBuf) {
-    let binding = PathBuf::from(from_path);
-    let file_name = binding.file_name().unwrap().to_str().unwrap();
-    let to_path = to_directory.join(file_name);
+pub fn remove_file(source_base_path: PathBuf, from_path: String, to_directory: PathBuf) {
+    let from_path = PathBuf::from(from_path);
+    let from_path = from_path.strip_prefix(&source_base_path).unwrap();
+    let destination_path = to_directory.join(from_path);
 
-    if to_path.exists() {
-        fs::remove_file(to_path).unwrap();
+    if from_path.is_file() {
+        fs::remove_file(destination_path).unwrap();
+    } else {
+        for entry in fs::read_dir(from_path).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+
+            remove_file(
+                source_base_path.clone(),
+                path.display().to_string(),
+                to_directory.clone(),
+            );
+        }
+
+        fs::remove_dir(destination_path).unwrap();
     }
 }
